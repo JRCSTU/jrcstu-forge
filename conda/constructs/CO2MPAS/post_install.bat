e1725f20e685bdac2f5b682d55fbc1b785654204
Kostis Anagnostopoulos <ankostis@gmail.com>
2019-06-12 14:00:12 +0200

    enh(cstor): delete all but conda-installed /pkgs (~400Mb)

--- conda/constructs/CO2MPAS/post_install.bat
+++ conda/constructs/CO2MPAS/post_install.bat
@@ -1,6 +1,6 @@
 ::@echo off
 :: Explicitly move noarch packages into `Lib/site-packages` as a workaround to
-:: [this issue][i86] with lack of `constructor` support for `noarch` packages.
+:: #86 with lack of `constructor` support for `noarch` packages.
 ::
 :: From https://github.com/openturns/otconda/blob/master/post_install.bat
 :: See also https://github.com/conda/constructor/issues/86#issuecomment-330863531
@@ -16,7 +16,12 @@ set co2progs=
 for /R pkgs %%i in (co2*.tar.bz2) do call set co2progs=%%co2progs%% "%%i"
 .\Library\bin\conda install -y %co2progs%
 
-:: DO NOT CLEAN packages kept by constructor.yaml: keep_pkgs: true
+:: Clean packages kept by constructor.yaml: keep_pkgs: true
+:: but keep those conda-installed, needed for the #86 workround, 
 :: so that menu-items can be rebuilt if another co2mpas is uninstalled.
-::rmdir /S/Q pkgs
-::md pkgs
+md pkgs.new
+xcopy /Y /F  pkgs\co2*.tar.bz2 pkgs.new\
+xcopy /Y /F pkgs\post_install.bat pkgs.new\
+:: Run them in a single pipe or else, 
+:: this files gets deleted, and move can't run!
+rd /s /q pkgs && move pkgs.new pkgs
