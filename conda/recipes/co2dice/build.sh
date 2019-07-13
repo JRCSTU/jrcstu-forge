d4641621540d291d7cc5f0f8d9f57c0f44730f4f
Kostis Anagnostopoulos <ankostis@gmail.com>
2019-07-13 06:48:30 +0200

    DROP(rcpy.co2dice): drop icons & manus from old co2dice...
    preparing recipe to be used for `co2mpas_dice`.

--- conda/recipes/co2dice/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p "$PREFIX/Menu"
-cp "$RECIPE_DIR/menu.json" "$PREFIX/Menu/co2dice.json"
-cp "$RECIPE_DIR/menu.ico" "$PREFIX/Menu/co2dice.ico"
-
-"$PYTHON" -m pip install . --no-deps --ignore-installed -vv || exit 1
