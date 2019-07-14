540d8d63c5a9e98cb3d0c90dc628e520457db782
Kostis Anagnostopoulos <ankostis@gmail.com>
2019-07-14 18:42:17 +0200

    FEAT(rcpy): retrofit CO2SIM --> co2mpas

--- conda/recipes/co2mpas/build.sh
+++ conda/recipes/co2mpas/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p "$PREFIX/Menu"
-cp "$RECIPE_DIR/menu.json" "$PREFIX/Menu/co2sim.json"
-cp "$RECIPE_DIR/menu.ico" "$PREFIX/Menu/co2sim.ico"
+cp "$RECIPE_DIR/menu.json" "$PREFIX/Menu/co2mpas.json"
+cp "$RECIPE_DIR/menu.ico" "$PREFIX/Menu/co2mpas.ico"
 
 "$PYTHON" -m pip install . --no-deps --ignore-installed -vv || exit 1
