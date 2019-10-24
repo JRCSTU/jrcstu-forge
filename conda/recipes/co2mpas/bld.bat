db61f355d43766d6f302f710685f9536100f248e
Stefano Corsi <stefano.corsi@gmail.com>
2019-10-24 22:36:48 +0200

    ENH(VER): bump 4.1.7-->4.1.8; ICONS

--- conda/recipes/co2mpas/bld.bat
+++ conda/recipes/co2mpas/bld.bat
@@ -1,6 +1,9 @@
 if not exist "%PREFIX%\Menu" mkdir "%PREFIX%\Menu"
 copy "%RECIPE_DIR%\menu.json" "%PREFIX%\Menu\co2mpas.json"
 copy "%RECIPE_DIR%\menu.ico" "%PREFIX%\Menu\co2mpas.ico"
+copy "%RECIPE_DIR%\www.ico" "%PREFIX%\Menu\www.ico"
+copy "%RECIPE_DIR%\folder.ico" "%PREFIX%\Menu\folder.ico"
+copy "%RECIPE_DIR%\console.ico" "%PREFIX%\Menu\console.ico"
 
 "%PYTHON%" -m pip install . --no-deps --ignore-installed -vv
 if errorlevel 1 exit 1
